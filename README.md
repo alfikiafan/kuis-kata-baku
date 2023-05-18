cdf39b985ddba658b7f86ebcbaf1db4e7223ed77
Alfiki Diastama Afan Firdaus <alfiki@student.uns.ac.id>
2023-05-18 13:33:26 +0700

    Add word list to popup

---
+++ README.md
@@ -0,0 +1,47 @@
+# Kuis Kata Baku
+
+Kuis Kata Baku adalah sebuah game interaktif yang bertujuan untuk menguji pemahaman pengguna terhadap kata baku dan tidak baku dalam bahasa Indonesia. Aplikasi ini dapat diakses melalui web browser dan terdiri dari 20 soal yang berisi pilihan kata yang harus dijawab dengan benar.
+
+## Fitur
+
+-   Pengguna dapat memilih salah satu jawaban yang benar dari dua pilihan kata yang disediakan
+-   Terdapat 20 soal yang harus dijawab dengan benar untuk menyelesaikan kuis
+-   Kata yang diujikan ditampilkan secara acak (ada 1000 lebih kemungkinan kata yang bisa ditampilkan)
+-   Setelah selesai, pengguna akan mendapatkan tampilan hasil pengerjaan dan dapat memulai kuis dari awal jika diinginkan
+-   Terdapat fitur untuk menyalakan/mematikan suara
+-   Terdapat fitur cek KBBI untuk mengecek makna kata yang muncul di soal
+-   Terdapat batasan waktu 15 detik setiap soal
+
+## Cara Penggunaan
+
+1.  Buka web browser dan akses Kuis Kata Baku.
+2.  Setelah aplikasi terbuka, pengguna dapat memilih untuk menyalakan atau mematikan suara dengan mengeklik tombol suara pada pojok kanan atas halaman.
+3.  Klik tombol "Mulai Kuis" untuk memulai kuis.
+4.  Untuk setiap soal, pilih jawaban yang menurut Anda adalah kata baku.
+5.  Setelah pengguna memilih jawaban, jika jawabannya benar maka akan muncul tampilan "Jawaban Benar", dan jika salah akan muncul tampilan "Jawaban Salah". Setelah itu, pengguna akan langsung diarahkan ke soal berikutnya setelah satu detik.
+7.  Setiap pertanyaan dalam game memiliki batasan waktu selama 15 detik. Pengguna diberikan kesempatan untuk mengecek arti kata dari pertanyaan sebelumnya selama sisa waktu pertanyaan masih tersedia dan pengguna belum memilih jawabannya. Sehingga, jika pengguna ingin mengetahui arti kata sebelumnya, jangan memilih jawaban selanjutnya sebelum selesai membaca arti kata.
+6.  Setelah menjawab semua soal, pengguna akan melihat tampilan hasil pengerjaan dan dapat memulai kuis dari awal jika menghendaki.
+
+## Teknologi yang Digunakan
+
+Teknologi Aplikasi ini menggunakan jQuery sebagai framework untuk mengatur interaksi dan manipulasi DOM. Data kata baku disimpan dalam format JSON dan diakses menggunakan fetch. CSS digunakan untuk mengatur tampilan dan layout dari aplikasi.
+
+## Kredit
+
+Aplikasi ini dibuat oleh Alfiki Diastama Afan Firdaus sebagai tugas  menggunakan jQuery dan data kata baku dari Lantip ([Lantip: Kumpulan Kata Baku dan Tidak Baku menurut KBBI](https://github.com/lantip/baku-tidak-baku)) dengan revisi menyesuaikan KBBI V terbaru. ([KBBI Daring](https://kbbi.kemdikbud.go.id/))
+
+## Lisensi
+
+Lisensi Aplikasi ini didistribusikan dengan lisensi MIT. Pengguna bebas menggunakan, mengubah, dan mendistribusikan aplikasi ini dengan syarat mencantumkan kredit kepada pembuat asli dan tidak menarik keuntungan finansial dari penggunaan aplikasi ini.
+
+## Kontribusi
+
+Jika Anda ingin mengusulkan entri kata baku, melaporkan adanya kesalahan kata, atau ingin mengajukan perubahan kode, silakan melakukan fork dan pull request ke repositori ini.
+
+## Kontak
+
+Jika terdapat masalah web, atau pertanyaan terkait aplikasi ini, silakan hubungi saya di alfiki.diastama@gmail.com.
+
+## Pembaruan
+
+Terakhir diperbarui pada 12 Mei 2023 01:30
---
+++ js/app.js
@@ -0,0 +1,233 @@
+$(function() {
+    let tidakBaku = [];
+    let baku = [];
+    const loading = $('.loading');
+    const mainContainer = $('.main-container');
+
+    fetch('https://raw.githubusercontent.com/alfikiafan/Kuis-Kata-Baku/master/data/data.json')
+        .then(response => response.json())
+        .then(jsonData => {
+            tidakBaku = Object.keys(jsonData);
+            baku = Object.values(jsonData);
+            setTimeout(() => {
+                loading.hide();
+                generateQuestion();
+                mainContainer.show();
+            }, 2000);
+        })
+        .catch(error => console.error('Error:', error));
+
+    const startButton = $('.btn-start');
+    const start = $('.start');
+    const kuis = $('.kuis');
+    const btn1 = $('.btn-1');
+    const btn2 = $('.btn-2');
+    const questionNumber = $('.question-number');
+    const questionButton = $('.btn-container');
+    const questionLoaderView = $('.question-loader');
+    const countDownText = $('.countdown');
+    const soundButton = $('.btn-sound');
+    const resetButton = $('.btn-reset');
+    const answerText = $('.answer-text');
+    const numRightAnswers = $('.right-count');
+    const numWrongAnswers = $('.wrong-count');
+    const rightSound = new Audio("audio/right.mp3");
+    const wrongSound = new Audio("audio/wrong.mp3");
+    const overlay = $('.overlay');
+    const scoreDisplay = $('#score');
+    const restartBtn = $('.btn-restart');
+
+    let correctAnswers = [];
+    let isCountdownStarted = false;
+    let countdown;
+    let timeLeft = 5;
+    let lastIndex = -1;
+    let index = -1;
+    let questionCount = 1;
+    let score = 0;
+    let rightCount = 0;
+    let wrongCount = 0;
+    let sound = true;
+
+    startButton.on('click', () => {
+        start.hide();
+        kuis.show();
+        countDown();
+        isCountdownStarted = true;
+    });
+    btn1.on('click', answer);
+    btn2.on('click', answer);
+
+    soundButton.on('click', () => {
+        const icon = soundButton.find('i');
+        icon.toggleClass('fa-volume-up fa-volume-mute');
+        sound = !sound;
+    });
+
+    resetButton.on('click', resetQuiz);
+    restartBtn.on('click', resetQuiz);
+
+    function wrongAnswerHandler(e, timeUp) {
+        timeLeft = 5;
+        const correctAnswer = baku[lastIndex];
+        wrongCount += 1;
+        answerText.css('color', '#f44336');
+        if (timeUp) {
+            answerText.text(`Waktu habis! Jawaban yang benar adalah "${correctAnswer}".`);
+        } else {
+            answerText.text(`Jawaban Anda, "${$(e.currentTarget).text()}", salah! Jawaban yang benar adalah "${correctAnswer}".`);
+        }
+        answerText.removeClass('right');
+        answerText.addClass('wrong');
+        numWrongAnswers.text(wrongCount);
+        if (sound) {
+            rightSound.pause();
+            rightSound.currentTime = 0;
+            wrongSound.play();
+        }
+    }
+
+    function countDown() {
+        clearInterval(countdown);
+        let timeUp = false;
+        let answerClicked = false;
+
+        function setAnswerClicked() {
+            answerClicked = true;
+            if (timeLeft > 0) {
+                timeLeft = 5;
+            }
+        }
+
+        btn1.on('click', setAnswerClicked);
+        btn2.on('click', setAnswerClicked);
+
+        countdown = setInterval(function() {
+            timeLeft--;
+            var minutes = Math.floor(timeLeft / 60).toString().padStart(2, '0');
+            var seconds = (timeLeft % 60).toString().padStart(2, '0');
+            var display = minutes + ":" + seconds;
+            countDownText.html(display);
+            if (timeLeft == 0) {
+                timeUp = true;
+                wrongAnswerHandler(answer, timeUp);
+                questionLoader();
+            }
+            if (answerClicked) {
+                timeLeft = 5;
+                answerClicked = false;
+                countDownText.html('00:05');
+                setTimeout(countDownText, 1000);
+            }
+        }, 1000);
+    }
+
+    function resetQuiz() {
+        clearInterval(countdown);
+        hidePopup();
+        kuis.hide();
+        start.show();
+        resetVariables();
+        generateQuestion();
+        correctAnswers = [];
+    }
+
+    function questionLoader() {
+        clearInterval(countdown);
+        if (questionCount <= 20) {
+            countDownText.html('00:05');
+            questionButton.hide();
+            questionLoaderView.show();
+            setTimeout(function() {
+                questionLoaderView.hide();
+                questionButton.show();
+                generateQuestion();
+                countDown();
+            }, 500);
+        }
+        else {
+            score = (rightCount / 20 * 100).toFixed(0);
+            showPopup(score);
+            isQuizEnded = true;
+        }
+    }
+
+    function generateQuestion() {
+        let isQuizEnded = false;
+        while (index === lastIndex) {
+            index = Math.floor(Math.random() * baku.length);
+        }
+        const rightButton = Math.floor(Math.random() * 2);
+        if (rightButton === 0) {
+            btn1.text(baku[index]);
+            btn2.text(tidakBaku[index]);
+        } else {
+            btn1.text(tidakBaku[index]);
+            btn2.text(baku[index]);
+        }
+        lastIndex = index;
+        if (questionCount <= 20) {
+            questionNumber.text(`Soal ${questionCount} dari 20`);
+            questionCount++;
+        }
+        if (isCountdownStarted && !isQuizEnded) {
+            countDown();
+        }
+    }
+
+    const resetVariables = () => {
+        isCountdownStarted = false;
+        timeLeft = 5;
+        lastIndex = -1;
+        index = -1;
+        questionCount = 1;
+        score = 0;
+        rightCount = 0;
+        wrongCount = 0;
+        countDownText.html('00:05');
+        answerText.text('Selamat belajar!');
+        answerText.css('color', '#546e7a');
+        numRightAnswers.text(rightCount);
+        numWrongAnswers.text(wrongCount);
+    }
+
+    function showPopup(score) {
+        scoreDisplay.text(score);
+        let linkText = "";
+        correctAnswers.forEach(function(correctAnswer, index) {
+            linkText += `<a href="https://kbbi.kemdikbud.go.id/entri/${correctAnswer}" target="_blank">${correctAnswer}</a>`;
+            if (index !== correctAnswers.length - 1) {
+                linkText += " · ";
+            }
+        });
+        $('#link-container').html(linkText);
+        overlay.show();
+        clearInterval(countdown);
+    }    
+
+    const hidePopup = () => {
+        overlay.hide();
+    };
+
+    function answer(e) {
+        const correctAnswer = baku[lastIndex];
+        correctAnswers.push(correctAnswer);
+        if ($(e.currentTarget).text() === baku[lastIndex]) {
+            rightCount += 1;
+            answerText.css('color', '#4caf50');
+            answerText.text(`Jawaban Anda, "${$(e.currentTarget).text()}", benar!`);
+            answerText.removeClass('wrong');
+            answerText.addClass('right');
+            numRightAnswers.text(rightCount);
+            if (sound) {
+                rightSound.pause();
+                rightSound.currentTime = 0;
+                rightSound.play();
+            }
+            questionLoader();
+        } else {
+            wrongAnswerHandler(e, false);
+            questionLoader();
+        }
+    }
+});
